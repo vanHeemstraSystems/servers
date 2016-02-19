b23b57b0365ff4d3c137d73fda8abdfb0b64d7b5
Willem van Heemstra <wvanheemstra@icloud.com>
2016-02-19 21:28:21 +0000

    Modified

--- creations_ecosystem_server.js
+++ creations_ecosystem_server.js
@@ -2,11 +2,14 @@
  * creations_ecosystem_server.js
  */
 
+// Babel ES6/JSX Compiler: needs to be at the top
+require('babel-register');
+
 // Core Node.js modules — path, querystring, http. 
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-//var bodyParser = require('body-parser'); // this throws an error about the inherit module not being found
+var bodyParser = require('body-parser');
 var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
@@ -17,7 +20,10 @@ require('babel-register');
 
 var swig  = require('swig');
 var React = require('../libraries/react');
-var ReactDOM = require('../libraries/react-dom/server');
+
+//var ReactDOM = require('../libraries/react-dom/server'); This doesn't find the module correctly
+var ReactDOM = require('react-dom/server');
+
 var Router = require('../libraries/react-router');
 var routes = require('../local_routes/creations_ecosystem_routes');
 
@@ -25,8 +31,8 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../publications')));
 
 
