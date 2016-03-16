519cb7d2154c9e7df1378b4090ff2f639774a82b
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-16 14:10:58 +0000

    Modified

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 /*
 * SERVER - The Server
 */
+
+'use strict'
+
+// PATH & PATHS - The Path(s): needs to be at the top
+var path = require('../libraries/path');
+var paths = require('../paths/paths');
+
 /*
 * APPLICATIONS - The Applications
 */
-var app = require('../applications/application.js');
+//OLD var app = require('../applications/application.js');
+var app = require(path.join(paths.applications, '/application.js'));
 /*
 * ENVIRONMENTS - The Environments
 * 
 * param: app
 */
-var environments = require('../environments/environment.js')(app);
+//OLD var environments = require('../environments/environment.js')(app);
+var environments = require(path.join(paths.environments, '/environment.js'))(app);
 /*
 * ROUTES - The Routes
 * 
 * param: app
 */
-var routes = require('../routes/routes.js')(app);
+//OLD var routes = require('../routes/routes.js')(app);
+var routes = require(path.join(paths.routes, '/routes.js'))(app);
 /*
 * LISTENERS - The Listeners (always put last)
 * 
 * param: app
 */
-var listeners = require('../listeners/listener.js')(app);
+//OLD var listeners = require('../listeners/listener.js')(app);
+var listeners = require(path.join(paths.listeners, '/listener.js'))(app);
